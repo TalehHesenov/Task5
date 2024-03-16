5147bd4c27d12163d975263b3bc2e7be8f3dd909
TalehHesenov <127445846+TalehHesenov@users.noreply.github.com>
2024-03-17 00:05:02 +0300

    new proje

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+$(function(){
+ $('.owl_carousel').owlCarousel({
+        margin:30,
+        dots:false,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+  });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
